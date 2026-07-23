3fb092e917808d48945cec104855a0bfc9b0a6fd
gitlab-admin <gitlabadmin@gitlab.com>
2026-07-23 18:51:48 +0530

    Auto commit: 2026-07-23 18:51:48

--- scripts/rename_facilities.go
+++ scripts/rename_facilities.go
@@ -10,21 +10,19 @@ import (
 func main() {
 	postgres.ConnectDB()
 
-	// Update PLC 192.169.4.152
-	if err := postgres.DB.Model(&models.MitsubishiPlc{}).Where("ip_address = ?", "192.169.4.152").Update("facility_name", "booth").Error; err != nil {
-		log.Fatal(err)
+	// Update PLC 192.169.4.152 -> booth
+	var plc1 models.MitsubishiPlc
+	if err := postgres.DB.Where("ip_address = ?", "192.169.4.152").First(&plc1).Error; err == nil {
+		postgres.DB.Model(&plc1).Update("facility_name", "booth")
+		postgres.DB.Model(&models.MitsubishiTagList{}).Where("plc_id = ?", plc1.ID).Update("facility_name", "booth")
+		fmt.Println("Updated PLC 192.169.4.152 facility name to 'booth'")
 	}
-	if err := postgres.DB.Model(&models.MitsubishiTagList{}).Where("plc_ip = ?", "192.169.4.152").Update("facility_name", "booth").Error; err != nil {
-		log.Printf("Error updating tags for 152: %v", err)
-	}
-	fmt.Println("Updated PLC 192.169.4.152 facility name to 'booth'")
 
-	// Update PLC 192.169.4.201
-	if err := postgres.DB.Model(&models.MitsubishiPlc{}).Where("ip_address = ?", "192.169.4.201").Update("facility_name", "oven").Error; err != nil {
-		log.Fatal(err)
+	// Update PLC 192.169.4.201 -> oven
+	var plc2 models.MitsubishiPlc
+	if err := postgres.DB.Where("ip_address = ?", "192.169.4.201").First(&plc2).Error; err == nil {
+		postgres.DB.Model(&plc2).Update("facility_name", "oven")
+		postgres.DB.Model(&models.MitsubishiTagList{}).Where("plc_id = ?", plc2.ID).Update("facility_name", "oven")
+		fmt.Println("Updated PLC 192.169.4.201 facility name to 'oven'")
 	}
-	if err := postgres.DB.Model(&models.MitsubishiTagList{}).Where("plc_ip = ?", "192.169.4.201").Update("facility_name", "oven").Error; err != nil {
-		log.Printf("Error updating tags for 201: %v", err)
-	}
-	fmt.Println("Updated PLC 192.169.4.201 facility name to 'oven'")
 }
